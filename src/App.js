2789c9e90068d6cca4fc1e776ef96849ae410801
Domenic Sita <domenicsita@Domenics-MacBook-Pro.local>
2021-05-24 23:26:24 -0500

    a --very-- simple calculator app made using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [display, setDisplay] = useState('');
+  let [result, setResult] = useState('');
+
+  const evaluateEquation = () => {
+    let ans = eval(display);
+    setDisplay('');
+    setResult(ans);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display-container">
+        <p className="display">{display}</p>
+        <p className="result">{result}</p>
+      </div>
+      <div className="row">
+        <button onClick={() => {setDisplay(display + '1')}}>1</button>
+        <button onClick={() => {setDisplay(display + '2')}}>2</button>
+        <button onClick={() => {setDisplay(display + '3')}}>3</button>
+        <button onClick={() => {setDisplay(''); setResult('')}}>C</button>
+      </div>
+      <div className="row">
+        <button onClick={() => {setDisplay(display + '4')}}>4</button>
+        <button onClick={() => {setDisplay(display + '5')}}>5</button>
+        <button onClick={() => {setDisplay(display + '6')}}>6</button>
+        <button onClick={() => {setDisplay(display + '+')}}>+</button>
+      </div>
+      <div className="row">
+        <button onClick={() => {setDisplay(display + '7')}}>7</button>
+        <button onClick={() => {setDisplay(display + '8')}}>8</button>
+        <button onClick={() => {setDisplay(display + '9')}}>9</button>
+        <button onClick={() => {setDisplay(display + '-')}}>-</button>
+      </div>
+      <div className="row">
+        <button onClick={() => {setDisplay(display + '0')}}>0</button>
+        <button onClick={() => {setDisplay(display + '*')}}>*</button>
+        <button onClick={() => {setDisplay(display + '/')}}>/</button>
+        <button onClick={evaluateEquation}>=</button>
+      </div>
     </div>
   );
 }
